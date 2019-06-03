89acc7163c3a7cb6bedec05b06b28d954531566f
Unknown <diego.lopez.garcia-ripoll@alumnos.upm.es>
2019-06-03 21:59:30 +0200

    Falta mutar y presentar
    ya llega casisiempre a la solución

--- src/genetic-learningD3/genetic.js
+++ src/genetic-learningD3/genetic.js
@@ -1,7 +1,11 @@
 
-function setPopulation(population, specimens){
-  population.specimens = specimens;
-}
+
+var TARGET = "Learning D3";
+
+
+///////////////////////////
+///// Public Functions/////
+///////////////////////////
 
 function new_population(mutationRate, pairingRate) {
     var population = {
@@ -13,16 +17,32 @@ function new_population(mutationRate, pairingRate) {
     return population;
 }
 
+function setPopulation(population, specimens){
+  population.specimens = specimens;
+}
+
 function iterate(population){
   population.generation ++;
+
   var winners = [];
   for(var i=0; i<population.specimens.length/2; i++){
     winners.push(selectSpecimens(population));
   }
-  winners.forEach((w) => console.log(w.name));
+
   population.specimens = pairWinners(winners);
+  population.specimens = population.specimens.concat(winners);
 }
 
+
+export var Population = {
+  setPopulation: setPopulation,
+  new_population: new_population,
+  iterate: iterate,
+}
+
+////////////////////////////
+///// Private Functions/////
+////////////////////////////
 
 function selectSpecimens(population){
   var fitnesees = [0];
@@ -44,18 +64,54 @@ function selectSpecimens(population){
     return population.specimens[i-1];
 }
 
-function pairWinners(){
-  console.log("pair winners not implemented");
-  return null;
-}
-export var Population = {
-  setPopulation: setPopulation,
-  new_population: new_population,
-  iterate: iterate,
+function pairWinners(winners){
+  var newSpecimens = new Array();
+
+  var middle = winners.length/2;
+  for(var i=0; i< middle; i++){
+    var childs = pair(winners[i], winners[middle + i]);
+    newSpecimens = newSpecimens.concat(childs);
+  }
+  return newSpecimens;
 }
 
 
-// Especimens
-export var specimen = function (name, fitness){
-  return {name: name, fitness: fitness};
+
+/*
+##################################################
+################### Especimens ###################
+##################################################
+*/
+export var newSpecimen = function (dna){
+  return {dna: dna, fitness: calcFitnes(dna)};
+}
+
+function pair(specimen1, specimen2){
+  var dna1FirstHalf = getDNAtillI(specimen1, TARGET.length / 2);
+  var dna1SecondHalf = getDNAfromI(specimen1, TARGET.length / 2);
+  var dna2FirstHalf = getDNAtillI(specimen2, TARGET.length / 2);
+  var dna2SecondHalf = getDNAfromI(specimen2, TARGET.length / 2);
+  var child1 = newSpecimen(dna1FirstHalf.concat(dna2SecondHalf))
+  var child2 = newSpecimen(dna2FirstHalf.concat(dna1SecondHalf))
+  return [child1,child2];
+}
+
+function calcFitnes(dna){
+  var fitness = 0;
+
+  for(var i=0; i<dna.length; i++){
+     if(dna[i] == TARGET[i]){
+       fitness ++;
+     }
+  }
+
+  return fitness;
+}
+
+function getDNAfromI(specimen, i){
+    return specimen.dna.slice(i);
+}
+
+function getDNAtillI(specimen, i){
+    return specimen.dna.slice(0, i);
 }
--- src/genetic-learningD3/index.js
+++ src/genetic-learningD3/index.js
@@ -1,9 +1,35 @@
 require('./index.css');
 
-import {Population, specimen} from "./genetic.js";
+import {Population, newSpecimen} from "./genetic.js";
 
 
 var population = Population.new_population(1, 1);
-var specimens = [specimen("juan", 0.6), specimen("Pedro", 0.4),specimen("Laura", 0.8), specimen("Vanesa", 0.2)];
+var specimens = [
+                  newSpecimen("le*rning D3"), newSpecimen("L*arni** D3"),
+                  newSpecimen("le*rni*g D3"), newSpecimen("Learni** D*"),
+                  newSpecimen("Le*rni*g*D3"), newSpecimen("**a*ni*g* D"),
+                  newSpecimen("learning D3"), newSpecimen("Learni** D3"),
+                  newSpecimen("Le*rni*g*D3"), newSpecimen("**a*ni*g* D"),
+                  newSpecimen("learning D3"), newSpecimen("Learni** D3"),
+
+                ];
 Population.setPopulation(population, specimens);
-Population.iterate(population);
+
+var generation = 0;
+var best = "";
+var bestFitness = 0;
+while (bestFitness < 11 && generation < 20){
+  Population.iterate(population);
+
+  population.specimens.forEach(
+    (e)=>{
+        if(e.fitness > bestFitness){
+          best = e.dna;
+          bestFitness = e.fitness;
+        }
+    }
+  );
+  generation++;
+}
+  console.log(best);
+  console.log(generation);
