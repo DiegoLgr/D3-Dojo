e98ff2e8ee4048c3b583102c296248d87b339a89
Unknown <diego.lopez.garcia-ripoll@alumnos.upm.es>
2019-06-04 15:09:38 +0200

    Animation is working.

--- src/genetic-learningD3/index.js
+++ src/genetic-learningD3/index.js
@@ -4,27 +4,31 @@ require('./index.css');
 import * as d3 from 'd3';
 import {Population, newSpecimen, TARGET} from "./genetic.js";
 
+var mutationRate = 0.1 // mutation rate should be less then 0.5 and greater than 0.
+var pairingRate = 0.3 // pair rate should can be whatever you want except 0.
+var population = Population.new_population(mutationRate, pairingRate);
 
-var population = Population.new_population(0.04, 1);
 var generation = 0;
 var best = "AprendiendoD3";
 var bestFitness = 0;
 
-var result = d3.select(".result")
+function setUp(){
+  var result = d3.select(".result")
 
-result.selectAll("h2")
-    .data([best])
-    .enter()
-    .append("h2")
-      .text((d)=>{return d});
+  result.selectAll("h2")
+      .data([best])
+      .enter()
+      .append("h2")
+        .text((d)=>{return d});
 
-result.selectAll("p")
-    .data([generation])
-    .enter()
-    .append("p")
-      .text((d)=>{return d});
+  result.selectAll("p")
+      .data([generation])
+      .enter()
+      .append("p")
+        .text((d)=>{return d});
+}
 
-while (bestFitness < TARGET.length && generation < 5000){
+function loop(){
   Population.iterate(population);
 
   population.specimens.forEach(
@@ -36,8 +40,12 @@ while (bestFitness < TARGET.length && generation < 5000){
     }
   );
   generation++;
-  updateDom(generation, best);
+  updateDom(generation, best)
+  if(bestFitness < TARGET.length && generation < 5000){
+    setTimeout(loop, 10);
+  }
 }
+
 
 function updateDom(generation, best){
   var result = d3.select(".result")
@@ -49,3 +57,9 @@ function updateDom(generation, best){
       .data([generation])
         .text((d)=>{return d});
 }
+
+// ->-> -> -> -> -> -> -> ->
+// -> -> ->   MAIN  -> -> ->
+// ->-> -> -> -> -> -> -> ->
+setUp();
+setTimeout(loop, 300);
